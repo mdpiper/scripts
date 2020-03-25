df2e7e8aee37d0bccc6bfc9141aef2c290fd26b8
Mark Piper <mark.piper@colorado.edu>
2020-03-25 11:06:04 -0600

    Set build type with a variable

--- local-build.sh
+++ local-build.sh
@@ -1,9 +1,11 @@
 #! /usr/bin/env bash
 # Script my typical cmake build process.
 
+build_type=Release
+
 mkdir -p _build && cd _build
 cmake .. \
-    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_BUILD_TYPE=$build_type \
     -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX
 make -s
 ctest
