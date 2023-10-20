0541867d5ad1436d2b59f5572bf0ae6763682fa5
Mark Piper <mark.piper@colorado.edu>
2023-10-20 10:54:59 -0600

    Check whether ctest is needed

--- local-build.sh
+++ local-build.sh
@@ -4,15 +4,22 @@
 build_type=Release
 
 mkdir -p _build && cd _build
+
 cmake .. \
     -DCMAKE_BUILD_TYPE=$build_type \
     -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX
+
 make -s
-ctest
+
+if test -n "$(find . -maxdepth 1 -name 'test*' -print -quit)"; then
+    ctest
+fi
+
 if [ $? != 0 ]; then
     exit 1
 else
     make install
 fi
+
 exit 0
 
